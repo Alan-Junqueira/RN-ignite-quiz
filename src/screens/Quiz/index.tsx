713240fd11a5bd2c8d64cb578db599ca697f4a4a
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-06-01 19:14:57 -0300

    feat: implement drag animation on question card

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -16,12 +16,15 @@ import { OutlineButton } from '../../components/OutlineButton';
 import Animated, { Easing, Extrapolate, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue, withSequence, withTiming } from 'react-native-reanimated';
 import { ProgressBar } from '../../components/ProgressBar';
 import { THEME } from '../../styles/theme';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 interface Params {
   id: string;
 }
 
 type QuizProps = typeof QUIZ[0];
+
+const CARD_INCLINATION = 10
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -30,8 +33,14 @@ export function Quiz() {
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
 
+  const { navigate } = useNavigation();
+
+  const route = useRoute();
+  const { id } = route.params as Params;
+
   const shake = useSharedValue(0)
   const scrollY = useSharedValue(0)
+  const cardPosition = useSharedValue(0)
 
   const shakeStyleAnimated = useAnimatedStyle(() => {
     return {
@@ -78,10 +87,29 @@ export function Quiz() {
     }
   })
 
-  const { navigate } = useNavigation();
+  const onPan = Gesture
+    .Pan()
+    .onUpdate(event => {
+      const moveToLeft = event.translationX < 0
+      if (moveToLeft) {
+        cardPosition.value = event.translationX
+      }
+    })
+    .onEnd(() => {
+      cardPosition.value = withTiming(0)
+    })
 
-  const route = useRoute();
-  const { id } = route.params as Params;
+  const dragStyles = useAnimatedStyle(() => {
+    const rotateZ = cardPosition.value / CARD_INCLINATION
+    return {
+      transform: [
+        { translateX: cardPosition.value, },
+        { rotateZ: `${rotateZ}deg` }
+      ]
+    }
+  })
+
+
 
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
@@ -156,6 +184,7 @@ export function Quiz() {
     }
   })
 
+
   useEffect(() => {
     const quizSelected = QUIZ.filter(item => item.id === id)[0];
     setQuiz(quizSelected);
@@ -196,15 +225,16 @@ export function Quiz() {
             totalOfQuestions={quiz.questions.length}
           />
         </Animated.View>
-
-        <Animated.View style={shakeStyleAnimated}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={[shakeStyleAnimated, dragStyles]}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />
