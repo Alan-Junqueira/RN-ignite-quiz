36a7073b5a5c906385258059d0665bb98d98fa5b
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-06-05 23:07:39 -0300

    feat: prevent default back navigation on question screen and finish screen

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import { Alert, Text, View, BackHandler } from 'react-native';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Audio } from 'expo-av'
@@ -225,6 +225,12 @@ export function Quiz() {
       handleNextQuestion();
     }
   }, [points]);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleStop)
+
+    return () => backHandler.remove()
+  }, [])
 
   if (isLoading) {
     return <Loading />
